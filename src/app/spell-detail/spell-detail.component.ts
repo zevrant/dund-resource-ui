7dc95691452671ff501dc43ab717f2bc1f901727
Zevrant <gerethd@gmail.com>
2018-08-22 21:02:05 -0400

    initial commit

---
+++ src/app/spell-detail/spell-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spell-detail',
+  templateUrl: './spell-detail.component.html',
+  styleUrls: ['./spell-detail.component.css']
+})
+export class SpellDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
